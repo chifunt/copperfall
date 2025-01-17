d5b8058f81f4e88dde8bc3d22145ff1ca7a0636c
Joshua Tjhie <chifunt@pm.me>
2025-01-17 14:15:05 +0100

    comment unnecessary actions

--- js/utils/Actions.js
+++ js/utils/Actions.js
@@ -1,8 +1,8 @@
 export const Actions = {
-    MOVE_UP: 'move_up',
-    MOVE_DOWN: 'move_down',
-    MOVE_LEFT: 'move_left',
-    MOVE_RIGHT: 'move_right',
+    // MOVE_UP: 'move_up',
+    // MOVE_DOWN: 'move_down',
+    // MOVE_LEFT: 'move_left',
+    // MOVE_RIGHT: 'move_right',
     HELP: 'help',
     BACK: 'back',
     PAUSE: 'pause',
