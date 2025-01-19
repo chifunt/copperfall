921a7bf97a5ffb6babda52e54e956c5b660e4e36
Joshua Tjhie <chifunt@pm.me>
2025-01-19 21:29:13 +0100

    make main menu scene open up main scene.

--- js/scenes/MainMenuScene.js
+++ js/scenes/MainMenuScene.js
@@ -2,10 +2,15 @@
 import { Scene } from "/js/core/Scene.js";
 import { Background } from "/js/gameobjects/Background.js";
 import { InputHandler } from "/js/gameobjects/InputHandler.js";
+import { Engine } from "../core/Engine.js";
+import { MainScene } from "./MainScene.js";
 
 export class MainMenuScene extends Scene {
   onStart() {
     const background = new Background();
     const inputHandler = new InputHandler();
+
+    // For now just load the main scene
+    Engine.instance.loadScene(new MainScene());
   }
 }
