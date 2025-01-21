8b1a22dd9e2ecef8533a3f0ae75b980458927fe8
Joshua Tjhie <chifunt@pm.me>
2025-01-21 03:33:57 +0100

    add some stuff to main menu scene

--- js/scenes/MainMenuScene.js
+++ js/scenes/MainMenuScene.js
@@ -4,6 +4,8 @@ import { InputHandler } from "/js/gameobjects/InputHandler.js";
 import { Engine } from "../core/Engine.js";
 import { MainScene } from "./MainScene.js";
 import { UIManager } from "../gameobjects/UIManager.js";
+import { Pickup } from "../gameobjects/Pickup.js";
+import { TestThing2 } from "../gameobjects/TestThing2.js";
 
 export class MainMenuScene extends Scene {
   onStart() {
@@ -11,7 +13,29 @@ export class MainMenuScene extends Scene {
     const inputHandler = new InputHandler();
     const uiManager = new UIManager();
 
+    uiManager.openMainMenu();
+
+    new Pickup({x: 50, y: -100});
+    new Pickup({x: 100, y: -50});
+    new Pickup({x: 150, y: 0});
+    new Pickup({x: 200, y: 50});
+    new Pickup({x: 250, y: 100});
+    new Pickup({x: -50, y: -100});
+    new Pickup({x: -100, y: -50});
+    new Pickup({x: -150, y: 0});
+    new Pickup({x: -200, y: 50});
+    new Pickup({x: -250, y: 100});
+    new TestThing2(0, 250, 900, 10);
+    new TestThing2(0, -250, 900, 10);
+    new TestThing2(440, 0, 10, 500);
+    new TestThing2(-440, 0, 10, 500);
+
     // For now just load the main scene
+    this.loadMainScene();
+  }
+
+  loadMainScene() {
+    UIManager.getInstance().closeMainMenu();
     Engine.instance.loadScene(new MainScene());
   }
 }
