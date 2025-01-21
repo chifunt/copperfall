b8818699a9cbaee9eba11ea0006f3a0e093ec5b6
Joshua Tjhie <chifunt@pm.me>
2025-01-21 14:27:53 +0100

    load main scene for faster testing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import { Engine } from "/js/core/Engine.js";
-import { MainMenuScene } from "/js/scenes/MainMenuScene.js"
+import { MainMenuScene } from "/js/scenes/MainMenuScene.js";
+import { MainScene } from "./scenes/MainScene.js";
 
 const engine = new Engine("canvas");
 
 engine.debugMode = true;
 
-engine.loadScene(new MainMenuScene());
+// engine.loadScene(new MainMenuScene());
+engine.loadScene(new MainScene());
 engine.start();
