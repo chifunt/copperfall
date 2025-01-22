868997fdfd3d746dac2eb7b3e291c37d9ea439c8
Joshua Tjhie <chifunt@pm.me>
2025-01-22 06:50:05 +0100

    update enemy test to be satisfying to kill

--- js/gameobjects/EnemyTest.js
+++ js/gameobjects/EnemyTest.js
@@ -3,6 +3,8 @@ import { SpriteRenderer } from "../components/SpriteRenderer.js";
 import { EasingFunctions } from "../utils/easing.js";
 import { BoxCollider } from "../components/BoxCollider.js";
 import { CircleCollider } from "../components/CircleCollider.js";
+import { ParticleSystemObject } from "./ParticleSystemObject.js";
+import { InputHandler } from "./InputHandler.js";
 
 export class EnemyTest extends GameObject {
   constructor(posx = 0, posy = 0, width = 30, height = 40) {
@@ -18,6 +20,24 @@ export class EnemyTest extends GameObject {
     img.onload = () => {
       this.addComponent(new SpriteRenderer(img, { pivot: "bottom", zOrder: 3 }));
     };
+
+    this.dieBurst = new ParticleSystemObject("DieBurst", {
+      burst: true,
+      burstCount: 20,
+      spawnRadius: 40,
+      color: "rgba(255, 10, 10, .5)",
+      particleLifetime: 0.5,
+      sizeOverTime: true,
+      playOnWake: false,
+      loop: false,
+      duration: 0.7, // only needed if we want the system to auto-destroy after
+      startSize: 10,
+      minAngle: 0,
+      maxAngle: 360,
+      minInitialSpeed: 200,
+      maxInitialSpeed: 300
+    });
+    this.dieBurst.transform.position = this.transform.position;
 
     const mainCollider = new BoxCollider({
       width: this.transform.scale.x,
@@ -37,6 +57,8 @@ export class EnemyTest extends GameObject {
     triggerCollider.onTriggerStay = (other) => {
       if (other.gameObject.name === "Player") {
         if (other.gameObject.isDashing) {
+          this.dieBurst.playSystem();
+          InputHandler.getInstance().triggerRumble(200, 100, .1, .3);
           this.destroy();
           return;
         }
