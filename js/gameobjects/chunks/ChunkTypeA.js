98e3b43a3b940b7ca12fb16783261e107812026e
Joshua Tjhie <chifunt@pm.me>
2025-01-23 04:45:57 +0100

    remove comment

--- js/gameobjects/chunks/ChunkTypeA.js
+++ js/gameobjects/chunks/ChunkTypeA.js
@@ -4,7 +4,6 @@ export class ChunkTypeA extends BaseChunk {
   constructor(cx, cy) {
     super(ChunkTypeA.name, cx, cy, 400);
 
-    // this.loadChunkMap("/assets/images/chunk-maps/zipzop.png");
     this.loadChunkMap("/assets/images/chunk-maps/slashcoin.png");
   }
 }
